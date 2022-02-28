ef0784367dead51d374793e8fba8fe07cb6a9d49
Ankesh <ankeshsarangi@gmail.com>
2022-02-28 09:43:01 +0530

    add

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://admin:47grdXqHCIThdCcT@mern.rtgup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
